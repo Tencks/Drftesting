90d70f9c6792725a18635e430dee1df86808c991
TencksSama <robertinospini@hotmail.com>
2023-09-15 21:19:37 -0300

    000webhos

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
     'http://www.whitenoiseland.com.ar',
-    'alpiccdg2022.000webhostapp.com'
+    'http://alpiccdg2022.000webhostapp.com'
     
 ]
 
